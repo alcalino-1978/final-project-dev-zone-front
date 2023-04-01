58be6117a879a7836d20c4b588e25b88313bf507
carlosmavogaro <113494764+carlosmavogaro@users.noreply.github.com>
2023-04-01 16:02:47 +0200

    refactor: change style in detail and list offer

--- src/app/pages/home/components/list-cat/list-cat.component.ts
+++ src/app/pages/home/components/list-cat/list-cat.component.ts
@@ -1,40 +1,40 @@
-import { Component } from '@angular/core';
-import { CatApiService } from '@shared/services/catApi.service';
-import { CatModelAPI } from '../../../../models/developer.model';
-import { Router } from '@angular/router';
+// import { Component } from '@angular/core';
+// import { CatApiService } from '@shared/services/catApi.service';
+// import { CatModelAPI } from '../../../../models/developer.model';
+// import { Router } from '@angular/router';
 
-@Component({
-  selector: 'app-list-cat',
-  templateUrl: './list-cat.component.html',
-  styleUrls: ['./list-cat.component.scss']
-})
-export class ListCatComponent {
-  catList: CatModelAPI[] = []
-  isLoading: boolean = false;
+// @Component({
+//   selector: 'app-list-cat',
+//   templateUrl: './list-cat.component.html',
+//   styleUrls: ['./list-cat.component.scss']
+// })
+// export class ListCatComponent {
+//   catList: CatModelAPI[] = []
+//   isLoading: boolean = false;
 
-  constructor(
-    private catApiService : CatApiService,
-    private router: Router,
+//   constructor(
+//     private catApiService : CatApiService,
+//     private router: Router,
 
-  ) {}
+//   ) {}
 
-  ngOnInit(): void {
-    this.getCatList();
-  }
+//   ngOnInit(): void {
+//     this.getCatList();
+//   }
 
-  private getCatList(): void {
-    this.isLoading = true;
-    this.catApiService.getCats().subscribe(
-      (response: CatModelAPI[]) => {
-        this.catList = response;
-        // console.log(response)
-        this.isLoading = false;
-      }, (error) => {
-      });
-  }
-  getCatDetail(id:any){
-    console.log(id)
-    this.router.navigateByUrl(`/breeds/${id}`)
-  }
+//   private getCatList(): void {
+//     this.isLoading = true;
+//     this.catApiService.getCats().subscribe(
+//       (response: CatModelAPI[]) => {
+//         this.catList = response;
+//         // console.log(response)
+//         this.isLoading = false;
+//       }, (error) => {
+//       });
+//   }
+//   getCatDetail(id:any){
+//     console.log(id)
+//     this.router.navigateByUrl(`/breeds/${id}`)
+//   }
 
-}
+// }

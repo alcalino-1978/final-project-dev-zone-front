ca83a47bbde7e71a536850fdedbeb48e6279537b
alcalino-1978 <114095946+alcalino-1978@users.noreply.github.com>
2023-04-04 20:55:50 +0200

    deploy: url a vercel

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,28 +1,16 @@
-// export const environment = {
-//   production: true,
-//   urlBase: 'https://final-project-dev-zone-back.vercel.app/v1/',
-//   urlImage: 'https://cdn2.thecatapi.com/images/',
-//   urlUsers: 'https://final-project-node-ten.vercel.app/users/',
+const urlBase = 'https://final-project-dev-zone-back.vercel.app/v1';
 
-//   urlCompany: 'https://final-project-dev-zone-back.vercel.app/v1/companies/',
-//   urlDevelopers: 'https://final-project-dev-zone-back.vercel.app/v1/developers/',
-//   urlJobOffers: 'https://final-project-dev-zone-back.vercel.app/v1/joboffers/',
 
-//   UNSPLASH_API_KEY: process.env['NG_APP_UNSPLASH_API_KEY'],
-//   urlUnsplash: `https://api.unsplash.com/photos/random/?query=developer&orientation=landscape&client_id=${process.env['NG_APP_UNSPLASH_API_KEY']}`,
-//   DEEP_PL_API_KEY: '41628d45-0951-8d10-0921-34e67e2f5d60:fx',
-//   CATS_API_KEY: process.env['NG_APP_CATS_API_KEY'],
-//   USER_API_KEY: process.env['NG_APP_USER_API_KEY'],
-// };
 export const environment = {
-  production: true,
-  urlBase: 'http://localhost:3000/v1/',
+  production: false,
+  urlBase: `${urlBase}`,
   urlImage: 'https://cdn2.thecatapi.com/images/',
+  // urlUsers: 'http://localhost:3000/users/',
   urlUsers: 'https://final-project-node-ten.vercel.app/users/',
 
-  urlCompany: 'http://localhost:3000/v1/companies/',
-  urlDevelopers: 'http://localhost:3000/v1/developers/',
-  urlJobOffers: 'http://localhost:3000/v1/joboffers/',
+  urlCompany: `${urlBase}/companies/`,
+  urlDevelopers: `${urlBase}/developers/`,
+  urlJobOffers: `${urlBase}/joboffers/`,
 
   UNSPLASH_API_KEY: process.env['NG_APP_UNSPLASH_API_KEY'],
   urlUnsplash: `https://api.unsplash.com/photos/random/?query=developer&orientation=landscape&client_id=${process.env['NG_APP_UNSPLASH_API_KEY']}`,
